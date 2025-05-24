0e35c1c720e765380282ec5fc6607308b83b7f30
arpel <arpelboxes@yahoo.fr>
2025-05-24 16:00:41 +0200

    Adding simple reconfiguration flow

--- custom_components/aldes/config_flow.py
+++ custom_components/aldes/config_flow.py
@@ -46,6 +46,35 @@ class AldesFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
         return await self._show_config_form(user_input)
 
+    async def async_step_reconfigure(self, user_input=None):
+        """Handle a reconfiguration flow initialized by the user."""
+        self._errors = {}
+
+        #if self._async_current_entries():
+        #    return self.async_abort(reason="single_instance_allowed")
+
+        if user_input is not None:
+            valid = await self._test_credentials(
+                user_input[CONF_USERNAME], user_input[CONF_PASSWORD]
+            )
+            if valid:
+                return self.async_create_entry(
+                    title=user_input[CONF_USERNAME], data=user_input
+                )
+            self._errors["base"] = "auth"
+            return await self._show_config_form(user_input)
+
+        return self.async_show_form(
+            step_id="reconfigure",
+            data_schema=vol.Schema(
+                 {
+                     vol.Required(CONF_USERNAME, default=user_input[CONF_USERNAME]): str,
+                     vol.Required(CONF_PASSWORD, default=user_input[CONF_PASSWORD]): str,
+                 }
+             ),
+             errors=self._errors,
+        )
+
     async def _show_config_form(self, user_input):  # pylint: disable=unused-argument
         """Show the configuration form to edit location data."""
         return self.async_show_form(
